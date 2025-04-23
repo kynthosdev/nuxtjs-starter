955a6e8c890b6634b7d0edb691eca7a056e2ff4b
kynthosdev <kynthosdevelopement@gmail.com>
2025-04-23 19:34:12 +0200

    ci, tailwind and primevue to configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primeuix/themes/aura";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
